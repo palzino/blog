9fc940f7cadad54eb77ebe04e5b2baaab8db5df4
Palvir <gpalvir@gmail.com>
2023-01-28 18:52:25 +0000

    Update Blog.js

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link, NavLink } from "react-router-dom";
+import axios from 'axios';
 
-const blog = () => (
-    <div>
-        <p>Blog</p>
-    </div>
-);
+const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [featuredblogs, setFeatured] = useState([]);
 
-export default blog;
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`) 
+            } catch (err) {
+
+            }
+        }
+        fetchData();
+    }, []);
+
+
+};
+   
+export default Blog;
