cac5130b65b25721601346dc34cf72b28aaf8e9a
Palvir <gpalvir@gmail.com>
2023-01-28 14:40:01 +0000

    api working

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,11 +18,11 @@ from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from re import re
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('api/blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     
 
